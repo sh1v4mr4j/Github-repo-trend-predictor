cbf0c9b1af57e877ddad7954e0671d7f8dd50a49
sniggy10 <snigdhaansu@gmail.com>
2024-12-05 20:38:01 -0500

    Adding kafka streaming application

---
+++ kafka_streaming/github-consumer/python-consumer.py
@@ -0,0 +1,79 @@
+#import datetime
+from json import loads
+import csv
+import os
+from datetime import datetime
+# from faker import Faker
+from kafka import KafkaConsumer
+import boto3
+
+KAFKA_SERVER = "kafka:9092"
+KAFKA_TOPIC = "github-repo"
+
+s3 = boto3.client('s3', aws_access_key_id='<enteryouraccesskey', aws_secret_access_key='<enteryoursecretkey>')
+bucket_name = '<add your bucket name>'
+
+CSV_HEADERS = ['name', 'full_name', 'html_url', 'description', 'stars', 'forks', 'language', 'created_at', 'updated_at', 'open_issues']
+CSV_DIRECTORY =  "/app/csv_data"
+if not os.path.exists(CSV_DIRECTORY):
+    os.makedirs(CSV_DIRECTORY)
+
+print("Current working directory:", os.getcwd())
+
+def generate_csv_filename():
+    # Use a timestamp-based filename to ensure uniqueness
+    timestamp = datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S")
+    return os.path.join(CSV_DIRECTORY, f"stream_{timestamp}.csv")
+
+def write_csv(csv_filename, data):
+    with open(csv_filename, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=CSV_HEADERS)
+        writer.writeheader()
+        writer.writerows(data)
+
+def upload_to_s3(file_path, bucket, key):
+    try:
+        s3.upload_file(file_path, bucket, key)
+        print(f"File uploaded successfully: {key}")
+    except Exception as e:
+        print(f"Error uploading file: {e}")
+
+if __name__ == "__main__":
+    consumer = KafkaConsumer(KAFKA_TOPIC, 
+                            bootstrap_servers=KAFKA_SERVER, 
+                            group_id='groupdId-919292',
+                            auto_offset_reset='earliest',
+                            value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+    all_repo_data = []
+    print("Consumer started")
+    try:
+        for message in consumer:
+            repo_info = message.value
+
+            for repo in repo_info:
+                repo_data = {
+                    'name': repo.get('name'),
+                    'full_name': repo.get('full_name'),
+                    'description': repo.get('description'),
+                    'stars': repo.get('stargazers_count'),
+                    'forks': repo.get('forks_count'),
+                    'language': repo.get('language'),
+                    'created_at': repo.get('created_at'),
+                    'updated_at': repo.get('updated_at'),
+                    'open_issues': repo.get('open_issues_count')
+                }
+                all_repo_data.append(repo_data)
+            print(f"Writing {len(all_repo_data)} records to CSV.")
+            csv_filename = generate_csv_filename()
+            print(csv_filename)
+            write_csv(csv_filename, all_repo_data)
+            s3_key = f"csv_data/{os.path.basename(csv_filename)}"
+            upload_to_s3(csv_filename, bucket_name, s3_key)
+            all_repo_data = []
+    except KeyboardInterrupt:
+        print("Consumer stopped")
+    finally:
+        # Close the consumer
+        consumer.close()
+    
---
+++ kafka_streaming/kafka-producer/python-producer.py
@@ -0,0 +1,61 @@
+#import datetime
+import time
+import schedule
+import requests
+from json import dumps
+from datetime import datetime, timedelta
+
+from kafka import KafkaProducer
+
+KAFKA_SERVER = "kafka:9092"
+KAFKA_TOPIC = "github-repo"
+GITHUB_EVENTS_URL = "https://api.github.com/"
+
+# Initialize last fetched timestamp as one minute ago
+
+last_fetched_timestamp = (datetime.utcnow() - timedelta(minutes=2)).isoformat() + "Z"  # ISO 8601 format
+print(last_fetched_timestamp)
+
+query = f"pushed:>{last_fetched_timestamp} stars:>0"
+page = 1
+per_page = 100
+
+GITHUB_REPOS_URL = f"https://api.github.com/search/repositories?q={query}&sort=stars&order=desc&page={page}&per_page={per_page}"
+
+def gen_data():
+  global last_fetched_timestamp
+  
+  producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER, value_serializer=lambda x:dumps(x).encode('utf-8')) 
+  
+  try:
+    response = requests.get(GITHUB_REPOS_URL, headers={"If-Modified-Since": last_fetched_timestamp})
+    if response.status_code == 200:
+      repos = response.json()
+
+      
+          
+      # Update the last_fetched_timestamp
+      if 'items' in repos:
+        repo_list = repos['items']  # List of repositories
+        producer.send(KAFKA_TOPIC, repo_list)
+        repo_count = len(repos['items'])
+        print(f"Number of repositories fetched: {repo_count}")
+        
+    elif response.status_code == 304:
+      print("No new events since the last fetch.")
+      
+    else:
+      print(f"Error: {response.status_code}, {response.text}")
+    
+  except Exception as e:
+    print(f"Error: {e}")
+
+  producer.flush()
+
+if __name__ == "__main__":
+  gen_data()
+  schedule.every(2).minutes.do(gen_data) # Respect GitHub API rate limits
+  
+  while True:
+    schedule.run_pending()
+    time.sleep(0.5)
